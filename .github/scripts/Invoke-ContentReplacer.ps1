ef4237205ad9dd3a71ccf2c1482a49603848ff40
Jack Tracey <41163455+jtracey93@users.noreply.github.com>
2024-08-21 17:28:48 +0100

    Update Invoke-ContentReplacer.ps1

--- .github/scripts/Invoke-ContentReplacer.ps1
+++ .github/scripts/Invoke-ContentReplacer.ps1
@@ -1,7 +1,7 @@
 $readmeRaw = Get-Content 'README.md'
 $readmeRaw -replace '## mission', "| :arrows_counterclockwise: | This repo is a mirror of the open source [AzGovViz project](https://github.com/JulianHayward/Azure-MG-Sub-Governance-Reporting) maintained by Julian Hayward, other Microsoft employees, and community members. It is mirrored here with his permission, and this mirror strives to stay in sync with released upstream versions. Please consider making product improvement recommendations on the upstream repo to prevent divergence. Thank you. |`n|-----------|:--------------------------|`n`n## Mission"
 
-$readmeRaw -replace '# Azure Governance Visualizer aka AzGovViz', "# Azure Governance Visualizer aka AzGovViz`n`n[![OpenSSF Scorecard](https://api.scorecard.dev/projects/github.com/JulianHayward/Azure-MG-Sub-Governance-Reporting/badge)](https://scorecard.dev/viewer/?uri=github.com/JulianHayward/Azure-MG-Sub-Governance-Reporting)`n`n"
+$readmeRaw -replace '# Azure Governance Visualizer aka AzGovViz', "[![OpenSSF Scorecard](https://api.scorecard.dev/projects/github.com/JulianHayward/Azure-MG-Sub-Governance-Reporting/badge)](https://scorecard.dev/viewer/?uri=github.com/JulianHayward/Azure-MG-Sub-Governance-Reporting)`n`n# Azure Governance Visualizer aka AzGovViz`n`n"
 
 $readmeRaw | Set-Content 'README.md'
 Write-Host 'README.md has been updated with block for upstream notice.'
